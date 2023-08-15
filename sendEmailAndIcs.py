6615f8925d0cd2c37a04b185301911e486ac92b5
Guilherme Moreira da Silva <81277029+remix-pc@users.noreply.github.com>
2023-08-15 14:11:36 -0300

    Update sendEmailAndIcs.py

--- sendEmailAndIcs.py
+++ sendEmailAndIcs.py
@@ -4,7 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from pathlib import Path
 from email import encoders
-from icalendar import Calendar, Event, vCalAddress, vText
+from icalendar import Calendar, Event, vCalAddress, vText #You need install icalendar
 
 
 
